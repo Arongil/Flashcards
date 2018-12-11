551abccc6018332521904deea7a8f5f355101ab1
Laker <lakernewhouse@gmail.com>
2018-12-10 17:35:04 -0900

    Added example text files with English and Spanish Vocabulary.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static("C:\\Users\\Laker\\Desktop\\Laker\\Programs\\Flashcards"));
+app.use(express.static("C:\\Users\\laker\\Desktop\\Laker\\Programs\\Flashcards"));
 
 app.listen("2000");
 console.log("Working on 2000");
