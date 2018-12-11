08d5023098ed9240f7e62a1ac0cd9ae75888c98f
Laker <lakernewhouse@gmail.com>
2018-12-10 18:03:23 -0900

    Removed server.js.
    This remote branch did this so I'm doing it to keep this version up to date. (I don't know how to merge the branches.)

--- server.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-var app = express();
-
-app.use(express.static("C:\\Users\\laker\\Desktop\\Laker\\Programs\\Flashcards"));
-
-app.listen("2000");
-console.log("Working on 2000");
